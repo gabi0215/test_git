ce14293696a9686efa32ffa9201b8461e9979617
gabi0215 <tmdrkq0215@gmail.com>
2024-07-10 10:54:28 +0900

    test

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 def greet(name):
-    return 'Hello, ' + name # Bad formatting and missing type annotations
+    return "Hello, " + name  # Bad formatting and missing type annotations
+
 
 if __name__ == "__main__":
-    print(greet('World'))
+    print(greet("World"))
